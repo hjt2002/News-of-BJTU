457d7e9e46ad09c69b0b9d612913103bb352faae
hjt <1161297340@qq.com>
2023-05-24 14:26:35 +0800

    first commit

---
+++ src/test/java/com/example/newsofbjtu/NewsofBjtuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newsofbjtu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsofBjtuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
